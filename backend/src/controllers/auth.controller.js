92914a8c685c900898eec9d4146710c97591d546
kiranwaghamode <kiranwaghamode99@gmail.com>
2026-05-20 09:16:41 +0530

    Token expiry added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -87,7 +87,12 @@ async function loginUserController(req, res){
         }
     )
     
-    res.cookie("token", token)
+    res.cookie("token", token,{
+    httpOnly: true,
+    secure: true,          
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000      
+    })
     res.status(200).json({
         message: "user logged in successfully",
         user:{
